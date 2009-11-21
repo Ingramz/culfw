9bfc5bbbffc2dc0e2dee18f1093910ccbe8c675e
nobody <nobody@bfc058f7-7f6e-431f-8662-3fa65896f8eb>
2009-11-21 09:04:22 +0000

    This commit was manufactured by cvs2svn to create tag 'CUL_VER_133'.

---
+++ tags/CUL_VER_133/culfw/clib/ttydata.c
@@ -0,0 +1,63 @@
+#include "display.h"
+#include "ttydata.h"
+#include <avr/pgmspace.h>
+
+void (*input_handle_func)(void);
+
+
+rb_t TTY_Tx_Buffer;
+rb_t TTY_Rx_Buffer;
+static char cmdbuf[TTY_BUFSIZE+1];
+
+extern PROGMEM t_fntab fntab[];
+uint8_t
+callfn(char *buf)
+{
+  for(uint8_t idx = 0; ; idx++) {
+    uint8_t n = __LPM(&fntab[idx].name);
+    void (*fn)(char *) = (void (*)(char *))__LPM_word(&fntab[idx].fn);
+    if(!n)
+      break;
+    if(buf == 0) {
+      DC(' ');
+      DC(n);
+    } else if(buf[0] == n) {
+      fn(buf);
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void
+analyze_ttydata()
+{
+  static uint8_t cmdlen;
+  uint8_t ucCommand;
+    
+  while(TTY_Rx_Buffer.nbytes) {
+
+    ucCommand = rb_get(&TTY_Rx_Buffer);
+
+    if(ucCommand == '\n' || ucCommand == '\r') {
+
+      if(!cmdlen)       // empty return
+        continue;
+
+      cmdbuf[cmdlen] = 0;
+      if(!callfn(cmdbuf)) {
+        DS_P(PSTR("? ("));
+        DC(cmdbuf[0]);
+        DS_P(PSTR(" is unknown) Use one of"));
+        callfn(0);
+        DNL();
+      }
+      cmdlen = 0;
+
+    } else {
+      if(cmdlen < sizeof(cmdbuf)-1)
+        cmdbuf[cmdlen++] = ucCommand;
+    }
+  }
+}
+
