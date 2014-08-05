8d7ebe7fa99da9e0d034a691647678b48cc2ffcb
raspii <raspii@bfc058f7-7f6e-431f-8662-3fa65896f8eb>
2014-08-05 20:19:32 +0000

    new files for kopp Free-Control protocol. Does not run yet, put to server to have a backup

---
+++ culfw/clib/kopp-fc.h
@@ -0,0 +1,12 @@
+#ifndef _kopp_fc_H
+#define _kopp_fc_H
+#define MAX_kopp_fc_MSG 11
+extern uint8_t kopp_fc_on;
+// #define SOMFY_RTS_FRAME_SIZE 7
+
+/* public kopp function call */
+void kopp_fc_init(void);
+// void kopp_fc_task(void);
+void kopp_fc_func(char *in);
+
+#endif
