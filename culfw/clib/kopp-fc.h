22b2a39d5aa41ca21994a19dbaf92ee056b754aa
raspii <raspii@bfc058f7-7f6e-431f-8662-3fa65896f8eb>
2014-12-13 01:30:46 +0000

    Transmit code is considered now, also

--- culfw/clib/kopp-fc.h
+++ culfw/clib/kopp-fc.h
@@ -1,7 +1,7 @@
 #ifndef _kopp_fc_H
 #define _kopp_fc_H
-#define MAX_kopp_fc_MSG 11
-#define kopp_fc_Command_char 11			// amount of command line characters for kopp_fc (without "Kt", 2 for Kopp key code, 2 for timeout)
+#define MAX_kopp_fc_MSG 13
+#define kopp_fc_Command_char 13			// amount of command line characters for kopp_fc (without "Kt", 2 for Kopp key code, 6 for Transmitter Code, 5 for timeout)
 extern uint8_t kopp_fc_on;
 extern uint8_t blkctr; 
 // #define SOMFY_RTS_FRAME_SIZE 7
