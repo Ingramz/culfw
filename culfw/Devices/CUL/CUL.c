9d1a25687e9e73abee0186538990a51089839240
raspii <raspii@bfc058f7-7f6e-431f-8662-3fa65896f8eb>
2015-01-13 20:12:19 +0000

    use parmeter "k" instead of "K" for CUL (not CCD, yet !) modified RF parameters within kopp-fc.c

--- culfw/Devices/CUL/CUL.c
+++ culfw/Devices/CUL/CUL.c
@@ -77,7 +77,7 @@ const PROGMEM t_fntab fntab[] = {
   { 'E', rwe_func },
 #endif
 #ifdef HAS_KOPP_FC
-  { 'K', kopp_fc_func },
+  { 'k', kopp_fc_func },
 #endif
 #ifdef HAS_RAWSEND
   { 'G', rawsend },
