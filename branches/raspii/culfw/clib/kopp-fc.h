5015464ddad181ac521d068f5a7df58761618179
raspii <raspii@bfc058f7-7f6e-431f-8662-3fa65896f8eb>
2015-02-01 18:40:40 +0000

    use Unix (LF) File Format now

--- branches/raspii/culfw/clib/kopp-fc.h
+++ branches/raspii/culfw/clib/kopp-fc.h
@@ -4,7 +4,6 @@
 #define kopp_fc_Command_char 14			// amount of command line characters for kopp_fc (without "Kt", 2 for Kopp key code, 6 for Transmitter Code, 5 for timeout)
 extern uint8_t kopp_fc_on;
 extern uint8_t blkctr; 
-// #define SOMFY_RTS_FRAME_SIZE 7
 
 /* public kopp function call */
 void kopp_fc_init(void);
