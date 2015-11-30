870a6f9031c2233c2f67f4b124edec0204aef2e3
rudolfkoenig <rudolfkoenig@bfc058f7-7f6e-431f-8662-3fa65896f8eb>
2015-11-30 18:33:53 +0000

    rf_zwave.c: send+ack

--- trunk/culfw/clib/rf_zwave.c
+++ trunk/culfw/clib/rf_zwave.c
@@ -7,28 +7,32 @@
 #include "display.h"
 
 #include "rf_zwave.h"
+extern void gettime(void); // debugging
 
-uint8_t zwave_on = 0;
+uint8_t zwave_on=0;
+uint8_t zwave_hcid[5];  // HomeId (4byte) + CtrlNodeId (1byte)
+void zwave_doSend(uint8_t *msg, uint8_t hblen);
+
 #define MAX_ZWAVE_MSG 60
 
 const uint8_t PROGMEM ZWAVE_CFG[] = {
   CC1100_IOCFG2,    0x01, // 00 GDO2 pin config:               00:FIFOTHR or end
   CC1100_IOCFG0,    0x2e, // 02 GDO0 pin config:                  2e:three state
-  CC1100_FIFOTHR,   0x01, // 03 FIFO Threshhold                  01:RX:8, TX:61
+  CC1100_FIFOTHR,   0x01, // 03 FIFO Threshhold                   01:RX:8, TX:61
   CC1100_SYNC1,     0xaa, // 04 Sync word, high byte
   CC1100_SYNC0,     0x0f, // 05 Sync word, low byte
   CC1100_PKTLEN,    0xff, // 06 Packet length
   CC1100_PKTCTRL1,  0x00, // 07 Packet automation control         00:no crc/addr
   CC1100_PKTCTRL0,  0x00, // 08 Packet automation control         00:fixlen,fifo
   CC1100_FSCTRL1,   0x06, // 0B Frequency synthesizer control
-  CC1100_FREQ2,     0x21, // 0D Frequency control word, high byte       868.4MHz
+  CC1100_FREQ2,     0x21, // 0D Frequency control word, high byte 868.4MHz
   CC1100_FREQ1,     0x66, // 0E Frequency control word, middle byte
   CC1100_FREQ0,     0x66, // 0F Frequency control word, low byte
-  CC1100_MDMCFG4,   0xca, // 10 Modem configuration                  bW 101.5kHz
-  CC1100_MDMCFG3,   0x93, // 11 Modem configuration                     dr 40kHz
-  CC1100_MDMCFG2,   0x06, // 12 Modem configuration                 2-FSK/16sync
-  CC1100_MDMCFG1,   0x72, // 13 Modem configuration                  24 preamble
-  CC1100_DEVIATN,   0x35, // 15 Modem deviation setting                dev:4394Hz
+  CC1100_MDMCFG4,   0xca, // 10 Modem configuration               bW 101.5kHz
+  CC1100_MDMCFG3,   0x93, // 11 Modem configuration               dr 40kHz
+  CC1100_MDMCFG2,   0x06, // 12 Modem configuration               2-FSK/16sync
+  CC1100_MDMCFG1,   0x72, // 13 Modem configuration               24 preamble
+  CC1100_DEVIATN,   0x35, // 15 Modem deviation setting           dev:4394Hz
   CC1100_MCSM0,     0x18, // 18 Main Radio Cntrl State Machine config
   CC1100_FOCCFG,    0x16, // 19 Frequency Offset Compensation config
   CC1100_AGCCTRL2,  0x03, // 1B AGC control
@@ -76,6 +80,28 @@ rf_zwave_init(void)
   zccRX();
 }
 
+uint8_t
+zwave_forMe(uint8_t *msg)
+{
+  if(msg[0] == zwave_hcid[0] &&
+     msg[1] == zwave_hcid[1] &&
+     msg[2] == zwave_hcid[2] &&
+     msg[3] == zwave_hcid[3] &&
+     msg[8] == zwave_hcid[4])
+    return 1;
+  return 0;
+}
+
+uint8_t
+zwave_ckSum(uint8_t *msg, uint8_t len)
+{
+  uint8_t cs = 0xff;
+  while(len)
+    cs ^= msg[--len];
+  return cs;
+}
+
+
 void
 rf_zwave_task(void)
 {
@@ -83,65 +109,72 @@ rf_zwave_task(void)
 
   if(!zwave_on)
     return;
+  if(!bit_is_set( CC1100_IN_PORT, CC1100_IN_PIN ))
+    return;
 
-  if(bit_is_set( CC1100_IN_PORT, CC1100_IN_PIN )) {
+  LED_ON();
+  CC1100_ASSERT;
+  cc1100_sendbyte( CC1100_READ_BURST | CC1100_RXFIFO );
+  for(uint8_t i=0; i<8; i++) { // FIFO RX threshold is 8
+     msg[i] = cc1100_sendbyte( 0 ) ^ 0xff;
+  }
+  CC1100_DEASSERT;
 
+  uint8_t len=msg[7], off=8;
+  if(len < 9 || len > MAX_ZWAVE_MSG) {
+    rf_zwave_init();
+    return;
+  }
+
+  cc1100_writeReg(CC1100_PKTLEN, len );
+  for(uint8_t mwait=MAX_ZWAVE_MSG-8; mwait > 0; mwait--) {
+    my_delay_us(220); // 1 byte @ 40kBaud + 10% margin
+    uint8_t flen = cc1100_readReg( CC1100_RXBYTES );
+    if(flen == 0)
+      continue;
     CC1100_ASSERT;
     cc1100_sendbyte( CC1100_READ_BURST | CC1100_RXFIFO );
-    for(uint8_t i=0; i<8; i++) { // FIFO RX threshold is 8
-       msg[i] = cc1100_sendbyte( 0 ) ^ 0xff;
+    if(off+flen > len) {
+      rf_zwave_init();
+      return;
     }
+    for(uint8_t i=0; i<flen; i++)
+      msg[off++] = cc1100_sendbyte( 0 ) ^ 0xff;
     CC1100_DEASSERT;
-
-    uint8_t len=msg[7], off=8;
-    if(len >= 8 && len <= MAX_ZWAVE_MSG) {
-
-      cc1100_writeReg(CC1100_PKTLEN, len );
-      for(uint8_t mwait=MAX_ZWAVE_MSG-8; mwait > 0; mwait--) {
-        my_delay_us(220); // 1 byte @ 40kBaud + 10% margin
-        uint8_t flen = cc1100_readReg( CC1100_RXBYTES );
-        if(flen == 0)
-          continue;
-        CC1100_ASSERT;
-        cc1100_sendbyte( CC1100_READ_BURST | CC1100_RXFIFO );
-        if(off+flen > len)
-          flen = len-off;
-        for(uint8_t i=0; i<flen; i++)
-          msg[off++] = cc1100_sendbyte( 0 ) ^ 0xff;
-        CC1100_DEASSERT;
-        if(off >= len)
-          break;
-      }
-
-      zccRX();
-
-      DC('z');    // Do the checksum in FHEM
-      for(uint8_t i=0; i<len; i++)
-        DH2(msg[i]);
-      DNL();
-
-      cc1100_writeReg(CC1100_PKTLEN, 0xff );
-    }
-
-  }
-
-  switch(cc1100_readReg( CC1100_MARCSTATE )) {
-    case MARCSTATE_RXFIFO_OVERFLOW:
-      ccStrobe( CC1100_SFRX  );
-    case MARCSTATE_IDLE:
-      ccStrobe( CC1100_SIDLE );
-      ccStrobe( CC1100_SNOP  );
-      ccStrobe( CC1100_SRX   );
+    if(off >= len)
       break;
   }
+
+  uint8_t isOk = (zwave_on == 'm' ||
+                  (zwave_forMe(msg) && zwave_ckSum(msg,len-1)==msg[len-1]));
+
+  if(isOk) {
+    DC('z'); 
+    for(uint8_t i=0; i<len; i++)
+      DH2(msg[i]);
+    DNL();
+  }
+
+  if(zwave_on == 'r' && isOk) { // ACK
+    msg[8] = msg[4]; // src -> target
+    msg[4] = zwave_hcid[4]; // src == ctrlId
+    msg[5] = 0x03; // ??
+    msg[7] = 10;
+    msg[9] = zwave_ckSum(msg, 9);
+    my_delay_ms(1); // ??
+    zwave_doSend(msg, 10);
+
+  } else {
+    cc1100_writeReg(CC1100_PKTLEN, 0xff );
+    zccRX();
+  }
+
+  LED_OFF();
 }
 
 void
-zwave_send(char *in)
+zwave_doSend(uint8_t *msg, uint8_t hblen)
 {
-  uint8_t msg[MAX_ZWAVE_MSG];
-  uint8_t hblen = fromhex(in+1, msg, MAX_ZWAVE_MSG);
-
   LED_ON();
   cc1100_writeReg(CC1100_PKTLEN, hblen);
   ccTX();
@@ -161,14 +194,35 @@ zwave_send(char *in)
 }
 
 void
+zwave_send(char *in)
+{
+  uint8_t msg[MAX_ZWAVE_MSG];
+  uint8_t hblen = fromhex(in+2, msg, MAX_ZWAVE_MSG);
+  zwave_doSend(msg, hblen);
+}
+
+void
 zwave_func(char *in)
 {
-  if(in[1] == 'r') {                // Reception on
+  if(in[1] == 'r' || in[1] == 'm') {// Reception on: receive or monitor
     rf_zwave_init();
-    zwave_on = 1;
+    zwave_on = in[1];
 
   } else if(in[1] == 's') {         // Send
     zwave_send(in);
+
+  } else if(in[1] == 'i') {         // set homeId and ctrlId
+    if(in[2]) {
+      fromhex(in+2, zwave_hcid, 5);
+    } else {
+      DH2(zwave_hcid[0]);
+      DH2(zwave_hcid[1]);
+      DH2(zwave_hcid[2]);
+      DH2(zwave_hcid[3]);
+      DC(' ');
+      DH2(zwave_hcid[4]);
+      DNL();
+    }
 
   } else {                          // Off
     zwave_on = 0;
